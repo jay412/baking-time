cf9d9d47193f30610660817eeb07ecea8ebe03a0
Jordan Chau <jchau3@buffalo.edu>
2018-07-29 18:15:52 -0400

    initial files commit

---
+++ BakingTime/app/src/main/java/com/herokuapp/jordan_chau/bakingtime/BakingTimeActivity.java
@@ -0,0 +1,13 @@
+package com.herokuapp.jordan_chau.bakingtime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BakingTimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_baking_time);
+    }
+}
