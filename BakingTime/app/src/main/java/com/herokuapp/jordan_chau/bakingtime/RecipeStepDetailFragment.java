4617c344d3ea480a39501d902be012f517b41905
Jordan Chau <jchau3@buffalo.edu>
2018-08-08 16:30:51 -0400

    1) adding exoplayer

--- BakingTime/app/src/main/java/com/herokuapp/jordan_chau/bakingtime/RecipeStepDetailFragment.java
+++ BakingTime/app/src/main/java/com/herokuapp/jordan_chau/bakingtime/RecipeStepDetailFragment.java
@@ -1,4 +1,72 @@
 package com.herokuapp.jordan_chau.bakingtime;
 
-public class RecipeStepDetailFragment {
+import android.app.Fragment;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+
+public class RecipeStepDetailFragment extends Fragment {
+    private Context context;
+    private SimpleExoPlayer mExoPlayer;
+    private SimpleExoPlayerView mPlayerView;
+
+    public RecipeStepDetailFragment() {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_recipe_step_detail, container, false);
+
+        context = rootView.getContext();
+
+        mPlayerView = rootView.findViewById(R.id.playerView);
+
+        //load question mark as background image until ...
+        // mPlayerView.setDefaultArtwork(BitmapFactory.decodeResource(getResources(), R.drawable.question_mark));
+
+        //get Uri from recipe
+
+        //initializePlayer(uri);
+
+        return rootView;
+    }
+
+    private void initializePlayer(Uri mediaUri) {
+        if (mExoPlayer == null) {
+            //Create an instance of the ExoPlayer
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            mExoPlayer = ExoPlayerFactory.newSimpleInstance(context, trackSelector, loadControl);
+            mPlayerView.setPlayer(mExoPlayer);
+            //Prepare the MediaSource
+            //String userAgent = ...;
+            MediaSource mediaSource = new ExtractorMediaSource(mediaUri, new DefaultDataSourceFactory(context, null), new DefaultExtractorsFactory(), null, null);
+            mExoPlayer.prepare(mediaSource);
+            mExoPlayer.setPlayWhenReady(true);
+        }
+    }
+
+    private void releasePlayer() {
+        mExoPlayer.stop();
+        mExoPlayer.release();
+        mExoPlayer = null;
+    }
 }
--- BakingTime/app/src/main/java/com/herokuapp/jordan_chau/bakingtime/RecipeStepFragment.java
+++ BakingTime/app/src/main/java/com/herokuapp/jordan_chau/bakingtime/RecipeStepFragment.java
@@ -10,10 +10,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.herokuapp.jordan_chau.bakingtime.adapter.RecipeCardAdapter;
 import com.herokuapp.jordan_chau.bakingtime.adapter.RecipeStepAdapter;
 import com.herokuapp.jordan_chau.bakingtime.model.Step;
 
