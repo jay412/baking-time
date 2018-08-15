b78725b6c82192f9b86009ad157509d9d7aeb815
Jordan Chau <jchau3@buffalo.edu>
2018-08-15 16:58:12 -0400

    testing

---
+++ BakingTime/app/src/androidTest/java/com/herokuapp/jordan_chau/bakingtime/RecyclerViewRecipeCardTest.java
@@ -0,0 +1,15 @@
+package com.herokuapp.jordan_chau.bakingtime;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class RecyclerViewRecipeCardTest {
+
+    @Rule
+    public 
+}
